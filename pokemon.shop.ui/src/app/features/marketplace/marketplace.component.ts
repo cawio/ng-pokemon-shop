c07531d37204225b0e0da7801a82b172d65985da
Simon <sberner9@gmail.com>
2024-01-29 08:18:09 +0100

    feat: Add Sorting to Marketplace

--- pokemon.shop.ui/src/app/features/marketplace/marketplace.component.ts
+++ pokemon.shop.ui/src/app/features/marketplace/marketplace.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { PokemonStore } from '../../store/pokemon.store';
+import { Component, ViewChild, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Pokemon } from '../../Models/Pokemon';
+import { PokemonStore } from '../../store/pokemon.store';
 
 @Component({
   selector: 'app-marketplace',
@@ -12,19 +13,25 @@ import { Pokemon } from '../../Models/Pokemon';
     MatTableModule,
     MatIconModule,
     MatButtonModule,
+    MatSortModule,
   ],
   templateUrl: './marketplace.component.html',
   styleUrl: './marketplace.component.scss'
 })
 export class MarketplaceComponent {
   private readonly store = inject(PokemonStore)
-  pokemons = this.store.entities;
+  dataSource = new MatTableDataSource(this.store.entities());
   loading = this.store.loading;
   error = this.store.error;
   columnsToDisplay = ['icon', 'name', 'price', 'quantity', 'cart'];
 
-
   constructor() { }
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   addToCart(pokemon: Pokemon) {
     this.store.addToCart(pokemon);
